668275e39144f6aeadec2e82494c7203cbc25cf2
appgp05 <alberto.puig.parellada@gmail.com>
2025-10-30 10:22:35 +0100

    .

--- app/arrays/listadoProductos.php
+++ app/arrays/listadoProductos.php
@@ -1,30 +1,31 @@
 <?php
     $productos = [
-        'lechuga' => ['unidades' => 200,
-            'precio' => 0.90],
-        'tomates' =>['unidades' => 2000,
-            'precio' => 2.15],
-        'cebollas' =>['unidades' => 3200,
-            'precio' => 0.49],
-        'fresas' =>['unidades' => 4800,
-            'precio' => 4.50],
-        'manzanas' =>['unidades' => 2500,
-            'precio' => 2.10],
+        'lechuga' => ['unidades' => 200, 'precio' => 0.90],
+        'tomates' =>['unidades' => 2000, 'precio' => 2.15],
+        'cebollas' =>['unidades' => 3200, 'precio' => 0.49],
+        'fresas' =>['unidades' => 4800, 'precio' => 4.50],
+        'manzanas' =>['unidades' => 2500, 'precio' => 2.10],
     ];
-
     function mostrarFormulario(array $productos): string{
-        $html = "<form action=\"listadoProductos.php\" method=\"post\">";
-
+        $html = "";
+        $html .= "<form id=\"formularioProductos\" action=\"listadoProductos.php\" method=\"post\">";
+        $html .= "<div>";
+        $html .= "<h1>Formulario de compra</h1>";
+        $html .= "<p>Por favor introduzca los productos a comprar</p>";
+        $html .= "</div>";
+        $html .= "<div class=\"productos\">";
         foreach($productos as $key => $value){
+            $nombre = strtoupper($key);
             $html .= "
                 <div>
-                        <h1>$key</h1>
+                        <h1>$nombre</h1>
                         <p>{$value["unidades"]} unidades</p>
                         <p>{$value["precio"]}€</p>
                         <input type='number' name='unidadesProductos[$key]' value='0'>
                 </div>";
         }
-        $html .= "<input type='submit' name='submit' value='Comprar'>";
+        $html .= "</div>";
+        $html .= "<div class='boton'><input type='submit' name='submit' value='Comprar'></div>";
         $html .= "</form>";
 
         return $html;
@@ -33,16 +34,37 @@
         $html = "";
 
         $unidadesProductos = $_POST["unidadesProductos"];
+        foreach($unidadesProductos as $key => $value){
+            $unidadesProductos[$key] = filter_var($unidadesProductos[$key], FILTER_VALIDATE_INT);
+        }
         var_dump($unidadesProductos);
 
+        $html .= "<div id='facturaProductos'>";
+        $html .= "<h1>Factura</h1>";
+        $html .= "<div>";
+        $htmlProductos = "";
         foreach($unidadesProductos as $key => $value){
+            if($value > $productos[$key]["unidades"]){
+                $value = $productos[$key]["unidades"];
+            }
+            $nombre = strtoupper($key);
             $precioUnidad = $productos[$key]["precio"];
             $precioTotal = $productos[$key]["precio"]*$value;
 
             if($value > 0){
-                $html .= "<p> - $key * $value | {$precioUnidad}€/u | {$precioTotal}</p>";
+                $htmlProductos .= "<p> - $nombre * $value | {$precioUnidad}€/u | {$precioTotal}</p>";
             }
         }
+        if($htmlProductos != ""){
+            $html .= $htmlProductos;
+        } else {
+            $html .= "<p>No ha comprado ningún producto</p>";
+        }
+        $html .= "<form action='listadoProductos.php' method='post'>";
+        $html .= "<input type='submit' name='submit' value='Volver a comprar'>";
+        $html .= "</form>";
+        $html .= "</div>";
+        $html .= "</div>";
 
         return $html;
     }
@@ -50,16 +72,29 @@
         $html = "";
 
         $unidadesProductos = $_POST["unidadesProductos"];
+        foreach($unidadesProductos as $key => $value){
+            $unidadesProductos[$key] = filter_var($unidadesProductos[$key], FILTER_VALIDATE_INT);
+        }
 
+        $html .= "<div id=\"inventarioProductos\">";
+        $html .= "<h1>Inventario Productos</h1>";
+        $html .= "<div class=\"productos\">";
         foreach($productos as $key => $value){
-            $unidadesFinales = $value["unidades"] - $unidadesProductos[$key];
+            $nombre = strtoupper($key);
+            $unidadesFinales = 0;
+            if(($value["unidades"] - (int) $unidadesProductos[$key]) > 0){
+                $unidadesFinales = $value["unidades"] - (int) $unidadesProductos[$key];
+            }
             $html .= "
                 <div>
-                        <h1>$key</h1>
+                        <h1>{$nombre}</h1>
                         <p>{$unidadesFinales} unidades</p>
                         <p>{$value["precio"]}€</p>
                 </div>";
         }
+
+        $html .= "</div>";
+        $html .= "</div>";
 
         return $html;
     }
@@ -70,10 +105,11 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Document</title>
+    <link rel="stylesheet" href="listadoProductos.css">
 </head>
 <body>
     <?php
-        if (isset($_POST['submit'])){
+        if (isset($_POST['submit']) && $_POST['submit'] == "Comprar"){
             //Mostrar inventario y factura
             echo mostrarFactura($productos);
             echo mostrarInventario($productos);
