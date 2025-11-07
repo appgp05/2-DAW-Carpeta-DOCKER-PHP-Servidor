6cf8a6f3280a9f850243d9dfe75d922a5c92d1d5
appgp05 <alberto.puig.parellada@gmail.com>
2025-11-07 11:45:09 +0100

    .

--- app/poo/calculadora_racional/Racional.php
+++ app/poo/calculadora_racional/Racional.php
@@ -33,5 +33,76 @@
 //                    return "1/10";
 //            }
         }
+
+        public function descomponerFactorialmente(int $numero): Array{
+            $numeroActual = $numero;
+            $factores = [];
+
+            while($numeroActual > 1) {
+                for($i = 2; $i <= $numeroActual; ++$i){
+                    if($numeroActual % $i == 0){
+                        $numeroActual = $numero / $i;
+                        $factores[] = $i;
+                        echo "<p>$i,,,,,$numeroActual</p>";
+                        break;
+                    }
+                }
+            };
+            return $factores;
+        }
+
+        public function sumar(Racional $racional): Racional{
+            $denominador = $this->denominador * $racional->denominador;
+            $numerador = $this->numerador * $racional->denominador + $racional->numerador * $this->denominador;
+
+            return $this->simplificar(new Racional($numerador, $denominador));
+        }
+
+        public static function sumarEstatico(Racional $racional1, Racional $racional2): Racional{
+            $denominador = $racional1->denominador * $racional2->denominador;
+            $numerador = $racional1->numerador * $racional2->denominador + $racional2->numerador * $racional1->denominador;
+
+            return new Racional($numerador, $denominador);
+        }
+
+        public function restar(Racional $racional): Racional{
+            $denominador = $this->denominador * $racional->denominador;
+            $numerador = $this->numerador * $racional->denominador - $racional->numerador * $this->denominador;
+
+            return new Racional($numerador, $denominador);
+        }
+
+        public function multiplicar(Racional $racional): Racional{
+            $numerador = $this->numerador * $racional->numerador;
+            $denominador = $this->denominador * $racional->denominador;
+
+            return new Racional($numerador, $denominador);
+        }
+
+        public function dividir(Racional $racional): Racional{
+            $numerador = $this->numerador * $racional->denominador;
+            $denominador = $this->denominador * $racional->numerador;
+
+            return new Racional($numerador, $denominador);
+        }
+
+        public function simplificar(): Racional{
+            $numerador = $this->numerador;
+            $denominador = $this->denominador;
+            $mayor = abs($numerador) > abs($denominador) ? abs($numerador) : abs($denominador);
+            $menor = abs($numerador) < abs($denominador) ? abs($numerador) : abs($denominador);
+
+            echo $mayor, $menor;
+
+            for($i = $menor; $i >= 1; --$i){
+                if($mayor % $i == 0 && $menor % $i == 0){
+                    $numerador = $numerador / $i;
+                    $denominador = $denominador / $i;
+                    break;
+                }
+            }
+
+            return new Racional($numerador,$denominador);
+        }
     }
 ?>
--- app/poo/calculadora_racional/index.php
+++ app/poo/calculadora_racional/index.php
@@ -1,22 +1,67 @@
 <?php
     require_once ("Racional.php");
     $r1 = new Racional(5, 5);  // 5/5
+    $r1Simplificado = $r1->simplificar();
     $r2 = new Racional();                           // 1/1
+    $r2Simplificado = $r2->simplificar();
     $r3 = new Racional(25);               // 25/1
+    $r3Simplificado = $r3->simplificar();
     $r4 = new Racional(7, 9);  // 7/9
+    $r4Simplificado = $r4->simplificar();
     $r5 = new Racional("8/7");            // 8/7
+    $r5Simplificado = $r5->simplificar();
+    $suma = new Racional("8/5")->sumar(new Racional("10/5"));
+    $sumaSimplificado = $suma->simplificar();
+    $resta = new Racional("8/5")->restar(new Racional("10/5"));
+    $restaSimplificado = $resta->simplificar();
+    $multiplicacion = new Racional("8/4")->multiplicar(new Racional("12/3"));
+    $multiplicacionSimplificado = $multiplicacion->simplificar();
+    $division = new Racional("8/4")->dividir(new Racional("12/3"));
+    $divisionSimplificado = $division->simplificar();
+    $simplificar = new Racional("8/4")->simplificar(new Racional("14/28"));
 
     $htmlEchoRacionales = "";
-    $htmlEchoRacionales .= "<p>Valor del r1: $r1</p>";
-    $htmlEchoRacionales .= "<p>Valor del r2: $r2</p>";
-    $htmlEchoRacionales .= "<p>Valor del r3: $r3</p>";
-    $htmlEchoRacionales .= "<p>Valor del r4: $r4</p>";
-    $htmlEchoRacionales .= "<p>Valor del r5: $r5</p>";
+    $htmlEchoRacionales .= "<p>Valor del r1: $r1, simplificado: $r1Simplificado</p>";
+    $htmlEchoRacionales .= "<p>Valor del r2: $r2, simplificado: $r2Simplificado</p>";
+    $htmlEchoRacionales .= "<p>Valor del r3: $r3, simplificado: $r3Simplificado</p>";
+    $htmlEchoRacionales .= "<p>Valor del r4: $r4, simplificado: $r4Simplificado</p>";
+    $htmlEchoRacionales .= "<p>Valor del r5: $r5, simplificado: $r5Simplificado</p>";
+    $htmlEchoRacionales .= "<p>Valor de la suma: $suma, simplificado: $sumaSimplificado/p>";
+    $htmlEchoRacionales .= "<p>Valor de la resta: $resta, simplificado: $restaSimplificado</p>";
+    $htmlEchoRacionales .= "<p>Valor de la multiplicacion: $multiplicacion, simplificado: $multiplicacionSimplificado</p>";
+    $htmlEchoRacionales .= "<p>Valor de la division: $division, simplificado: $divisionSimplificado</p>";
+    $htmlEchoRacionales .= "<p>Valor de la simplificacion: $simplificar, simplificado: $multiplicacionSimplificado</p>";
+
+//    $a = $r5->descomponerFactorialmente(10);
+//    $htmlEchoRacionales .= "<p>Valor del a: $a</p>";
 
     $htmlEchoRacionales;
-    if(isset($_POST['submit'])){
-        $racional = new Racional($_POST['numeroRacional']);
-        $htmlEchoRacionales .= "<p>$racional</p>";
+
+    $submit = $_POST['submit']??null;
+
+    switch ($submit) {
+        case "Crear":
+            $racional = new Racional($_POST['numeroRacional']);
+            $htmlEchoRacionales .= "<p>$racional</p>";
+            break;
+        case "Calcular":
+            $racional1 = new Racional($_POST['racional1']);
+            $racional2 = new Racional($_POST['racional2']);
+            $operacion = $_POST['operacion'];
+            switch ($operacion) {
+                case "+":
+                    $resultadoOperacion = $racional1->sumar($racional2);
+                    break;
+                case "-":
+                    $resultadoOperacion = $racional1->restar($racional2);
+                    break;
+                case "*":
+                    $resultadoOperacion = $racional1->multiplicar($racional2);
+                    break;
+                case "/":
+                    $resultadoOperacion = $racional1->dividir($racional2);
+                    break;
+            }
     }
 ?>
 
@@ -31,8 +76,23 @@
     <?= $htmlEchoRacionales ?>
 
     <form action="index.php" method="post">
+        <h1>Crear un Racional nuevo</h1>
         <input type="text" name="numeroRacional">
-        <input type="submit" name="submit" value="Enviar">
+        <input type="submit" name="submit" value="Crear">
+    </form>
+    <form action="index.php" method="post">
+        <input type="text" name="racional1">
+        <select name="operacion" id="">
+            <option value="+">+</option>
+            <option value="-">-</option>
+            <option value="*">*</option>
+            <option value="/">/</option>
+        </select>
+        <input type="text" name="racional2">
+        <input type="submit" name="submit" value="Calcular">
+
+        <h1>Resultado:</h1>
+        <p><?= $resultadoOperacion??"" ?></p>
     </form>
 </body>
 </html>
